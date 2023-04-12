51ab398387c8a5ec9d0fa4865823e0bd88508621
Mike Huang <wellwind@mail2000.com.tw>
2023-04-12 21:10:46 +0800

    使用 Angular CLI 轉換成 standalone 應用程式
    ng generate @angular/core:standalone --mode=standalone-bootstrap --path=./

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
+    selector: 'app-root',
+    template: `
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
       <h1>
@@ -25,7 +25,8 @@ import { Component } from '@angular/core';
     </ul>
     
   `,
-  styles: []
+    styles: [],
+    standalone: true
 })
 export class AppComponent {
   title = 'study4-2023-standalone-angular';
