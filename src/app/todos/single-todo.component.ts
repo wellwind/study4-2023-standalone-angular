4cc1685303febb37fea14d17d1af96587123bbc1
Mike Huang <wellwind@mail2000.com.tw>
2023-04-12 21:19:16 +0800

    step1: 建立相關元件與檔案

---
+++ src/app/todos/single-todo.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-single-todo',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      single-todo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SingleTodoComponent {
+
+}
