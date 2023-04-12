0eff22472fd09ef2c94860ed22044f4f41eb1481
Mike Huang <wellwind@mail2000.com.tw>
2023-04-12 21:40:28 +0800

    step 3: 使用 provideRouter 設定路由

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideRouter([
+      {
+        path: '',
+        redirectTo: 'todos',
+        pathMatch: 'full',
+      },
+      {
+        path: 'todos',
+        loadChildren: () => import('./app/todos/routes'),
+      }
+    ])
+  ],
+}).catch((err) => console.error(err));
